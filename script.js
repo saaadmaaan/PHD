83213f9d10cba32f93957bb08f24b4a093d1a887
saaadmaaan <167014027+saaadmaaan@users.noreply.github.com>
2024-07-27 16:17:25 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.querySelector('.text').addEventListener('click', () => {
-    const textElement = document.querySelector('.text');
-    textElement.style.animation = 'none';
-    textElement.offsetHeight; /* trigger reflow */
-    textElement.style.animation = '';
-    textElement.style.animation = 'jump 3s ease-in-out infinite';
-    
-});
